9ef3c567d681514b554e21d79b88cd6b64e18c4f
Rohan Baddi <ronbaddi004@gmail.com>
2017-12-22 23:48:27 +0530

    First Commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn ronpro.wsgi
---
+++ feedapp/admin.py
@@ -0,0 +1,12 @@
+from .forms import ContactForm
+
+from django.contrib import admin
+from .models import Feedbaack
+# Register your models here.
+
+class FormAds( admin.ModelAdmin ):
+    form = ContactForm
+    # list_display = Feedbaack._meta.get_fields()
+
+
+admin.site.register(Feedbaack, FormAds)
